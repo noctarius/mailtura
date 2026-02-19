4f479a57257d655ce2afc384dfc6065a87ce291d
Christoph Engelbert (noctarius) <me@noctarius.com>
2026-02-19 07:18:03 +0100

    Automatically update the api url when running in production mode

--- packages/frontend/src/constants.ts
+++ packages/frontend/src/constants.ts
@@ -1,1 +1,14 @@
-export const API_URL = "http://localhost:3333/";
+const makeApiUrl = () => {
+  const location = window.location;
+  const protocol = location.protocol;
+  const host = location.hostname;
+  const port = location.port;
+  if ((protocol === "http" && port === "80") || (protocol === "https" && port === "443")) {
+    return `${protocol}://${host}/`;
+  }
+  return `${protocol}//${host}:${port}/`;
+};
+
+const configuredApiUrl = import.meta.env.MODE === "production" ? makeApiUrl() : "http://localhost:3333/";
+
+export const API_URL = configuredApiUrl.endsWith("/") ? configuredApiUrl : `${configuredApiUrl}/`;
